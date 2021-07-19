5a3a17471b74c6b8e28bdca5366c855967095618
Metaleakbal <87192354+Metaleakbal@users.noreply.github.com>
2021-07-19 04:21:35 -0500

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+   apiKey: "AIzaSyD4k9gH6MN7XzdapqpBx7JdaA939wllkBo",
+    authDomain: "chat41e-d29fa.firebaseapp.com",
+    projectId: "chat41e-d29fa",
+    storageBucket: "chat41e-d29fa.appspot.com",
+    messagingSenderId: "691001676038",
+    appId: "1:691001676038:web:e197454e1b9d40b7344e87",
+    measurementId: "G-XZTT7B2B4R"
+    });
